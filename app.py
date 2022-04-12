7cfc0280f5e3c1bb735e209fbaf058515faec4ec
omerscofield31 <88977835+omerscofield31@users.noreply.github.com>
2022-04-12 12:54:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,19 +9,8 @@ def index():
 @app.route("/api")
 def nodemcu_get():
     doviz = requests.get('https://api.genelpara.com/embed/doviz.json')
-    #altin = requests.get('https://api.genelpara.com/embed/altin.json')
-    #py_altin = json.loads(altin)
-    py_doviz = json.loads(doviz.content)
 
-    jsons = {
-    "1":{"1" : "Dolar/Tl",
-    "2" : (py_doviz["USD"])["alis"],
-    },
-    "2":{"1" : "Gram/Tl",
-    "2" : (py_doviz["USD"])["alis"],
-    }
-}
-    return json.dumps(jsons)
+    return doviz.content
 
 if __name__ == "__main__":
     app.run()
