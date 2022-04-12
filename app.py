8f73b50bc6b3d11bf15b1e58e7a69686ad46576b
omerscofield31 <88977835+omerscofield31@users.noreply.github.com>
2022-04-12 15:26:06 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import json
+import requests
+
+app = Flask(__name__)
+doviz = requests.get('https://api.genelpara.com/embed/doviz.json')
+#altin = requests.get('https://api.genelpara.com/embed/altin.json')
+#py_altin = json.loads(altin)
+py_doviz = json.loads(doviz.content)
+
+jsons = {
+    "1":{"1" : "Dolar/Tl",
+    "2" : (py_doviz["USD"])["alis"],
+    },
+    "2":{"1" : "Gram/Tl",
+    "2" : (py_doviz["USD"])["alis"],
+    }
+}
+
+print(json.dumps(jsons))
+@app.route("/api")
+def nodemcu_get():
+    return json.dumps(jsons)
+
+if __name__ == "__main__":
+    app.run()
